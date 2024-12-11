4ad898aa5d96dd39dd46174305b2a199f3335554
spearkkk <spearkkp@gmail.com>
2024-12-12 01:09:59 +0900

    Change page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,37 @@
-import React from 'react';
-import InstallationSection from "@/app/components/InstallationSection";
-import FeaturesSection from "@/app/components/FeaturesSection";
-import Header from "@/app/components/Header";
-import Footer from "@/app/components/Footer";
+"use client";
+
+import React, { useEffect } from 'react';
 
 export default function Home() {
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollTop = window.scrollY;
+            const body = document.body;
+            if (scrollTop > 100) {
+                body.classList.add('dimmed');
+            } else {
+                body.classList.remove('dimmed');
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
     return (
-        <div className="bg-gray-50 text-gray-900 flex flex-col min-h-screen">
-            <Header />
-            <main className="flex-grow flex justify-center items-center">
-                <div className="w-full max-w-4xl text-center">
-                    <FeaturesSection />
-                    <InstallationSection />
+        <div className="flex flex-col min-h-screen relative">
+            <span className="top-right-link">
+            <a href="https://unsplash.com/ko/%EC%82%AC%EC%A7%84/%EC%88%98%EC%A4%91-%EC%82%AC%EC%A7%84-1braZySlEKA?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
+                Photo by Jeremy Bishop on Unsplash
+            </a>
+            </span>
+            <main className="flex-grow flex flex-col justify-center items-center p-4">
+                <div className="w-full max-w-4xl text-center flex flex-col justify-end items-center min-h-screen">
+                    hello world<br/>
+                    hello world<br/>
+                    hello world<br/>
                 </div>
             </main>
-            <Footer />
         </div>
     );
 }
